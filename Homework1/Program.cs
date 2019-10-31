346fd272e9391e8024cf135e6056c0ae3aef223c
Rayle1gh <darkmaxim003@gmail.com>
2019-10-31 14:06:56 +0300

    1

---
+++ Homework1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading;
+
+namespace Homework1
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите имя:");
+            string name = Console.ReadLine();
+            Thread.Sleep(5000);
+            Console.WriteLine($"Привет, {name}!");
+            Thread.Sleep(5000);
+            Console.WriteLine("Пока, {0}", name);
+            Console.ReadKey();
+        }
+    }
+}
