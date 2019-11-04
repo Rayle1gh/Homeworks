5a11985fab2c0d4b5db15bdbd61a4ad3af66b2aa
Rayle1gh <darkmaxim003@gmail.com>
2019-11-04 21:44:05 +0300

    Add homework2

---
+++ Homework2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Homework2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x;
+            double y;
+            Console.WriteLine("Введите первое число:");
+            x = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите второе число:");
+            y = Convert.ToDouble(Console.ReadLine());
+            var a = x + y;
+            var b = x - y;
+            var c = x * y;
+            Console.WriteLine("Сумма чисел = {0}", a);
+            Console.WriteLine("Разница чисел = {0}", b);
+            Console.WriteLine("Произведение чисел = {0}", c);
+            Console.ReadKey();
+        }
+    }
+}
