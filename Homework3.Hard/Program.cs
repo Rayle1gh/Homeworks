e19cb32ca7104acb98c3f2801444f25102d60a00
Rayle1gh <darkmaxim003@gmail.com>
2019-11-08 13:11:58 +0300

    Full Homework3

---
+++ Homework3.Hard/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Homework3.Hard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int[] b = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+            Console.Write("*\t");
+            for (int i = 0; i < a.Length; i++)
+            {
+                Console.Write(a[i] + "\t");
+            }
+            Console.WriteLine();
+            for (int i = 0; i < b.Length; i++)
+            {
+                Console.Write(b[i] + "\t");
+
+                for (int j = 0; j < a.Length; j++)
+                {
+                    Console.Write(a[j] * b[i] + "\t");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Homework3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> names = new List<string>();
+            List<byte> ages = new List<byte>();
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write("Введите имя {0}: ", i);
+                names.Add(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("Введите возраст {0}: ", names[i]);
+                byte age = Convert.ToByte(Console.ReadLine());
+                ages.Add(age);
+            }
+
+            for (int i = 0; i < names.Count; i++)
+            {
+                Console.WriteLine("Имя: {0}, Возраст: {1}", names[i], ages[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
