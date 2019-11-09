d90768f5ba5c1b59088b7cfa2639ad7696212d58
Rayle1gh <darkmaxim003@gmail.com>
2019-11-09 20:02:05 +0300

    Homework3.Hard FIX

--- Homework3.Hard/Program.cs
+++ Homework3.Hard/Program.cs
@@ -6,22 +6,33 @@ namespace Homework3.Hard
     {
         static void Main(string[] args)
         {
-            int[] a = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-            int[] b = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-
-            Console.Write("*\t");
-            for (int i = 0; i < a.Length; i++)
+            Console.WriteLine("Введите первое число: ");
+            int a = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int b = int.Parse(Console.ReadLine());
+            int[] nums = new int[a];
+            int[] numsTwo = new int[b];
+            for (int i = 0; i < a; i++)
             {
-                Console.Write(a[i] + "\t");
+                nums[i] = i + 1;
+            }
+            for (int i = 0; i < b; i++)
+            {
+                numsTwo[i] = i + 1;
+            }
+            Console.Write("*\t");
+            for (int j = 1; j < numsTwo.Length; j++)
+            {
+                Console.Write(numsTwo[j] + "\t");
             }
             Console.WriteLine();
-            for (int i = 0; i < b.Length; i++)
+            for (int i = 1; i < nums.Length; i++)
             {
-                Console.Write(b[i] + "\t");
+                Console.Write(nums[i] + "\t");
 
-                for (int j = 0; j < a.Length; j++)
+                for (int j = 1; j < numsTwo.Length; j++)
                 {
-                    Console.Write(a[j] * b[i] + "\t");
+                    Console.Write(nums[i] * numsTwo[j] + "\t");
                 }
                 Console.WriteLine();
             }
