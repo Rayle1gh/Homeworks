d6a8474237c220ab29256ca4d42b69137f7984bc
Rayle1gh <darkmaxim003@gmail.com>
2019-11-19 13:05:51 +0300

    ДЗ 6

---
+++ Homework6.1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Homework6._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsWork = true;
+            while (IsWork)
+            {
+                try
+                {
+                    Console.WriteLine("Введите положительное натуральное число не более 2 миллиардов(exit для выхода): ");
+                    string str = Console.ReadLine();
+                    if (str == "exit")
+                        IsWork = false;
+                    int i = 0;
+                    int num = int.Parse(str);
+
+                    if (num <= 0)
+                    {
+                        Console.WriteLine("Введено неверное значение! Попробуйте ещё раз: ");
+                        continue;
+                    }
+                    foreach (char ch in str)
+                    {
+                        if ((int)ch % 2 == 0)
+                        {
+                            i++;
+                        }
+                    }
+                    Console.WriteLine("В числе {0} - {1} четных цифр", num, i);
+                }
+                catch (OverflowException e)
+                {
+                    Console.WriteLine($"Ошибка {e.GetType()}! Попробуйте ещё раз: ");
+                }
+                catch (FormatException e)
+                {
+                    Console.WriteLine($"Ошибка {e.GetType()}! Попробуйте ещё раз: ");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+    }
+}
---
+++ Homework6.2/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace Homework6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                while (true)
+                {
+                    Console.Clear();
+
+                    double contribution = SetParam("Введите сумму первоначального взноса в рублях: ");
+                    double percent = SetParam("Введите ежедневный процент дохода в виде десятичной дроби (1% = 0.01): ");
+                    double result = SetResult(contribution);
+
+                    PrintResultDays(contribution, percent, result);
+
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void PrintResultDays(double contribution, double percent, double result)
+        {
+            int days = 0;
+
+            while (contribution <= result)
+            {
+                contribution *= (percent + 1);
+                days++;
+            }
+
+            Console.WriteLine($"Необходимое количество дней для накопления желаемой суммы: {days}");
+        }
+
+        private static double SetResult(double contribution)
+        {
+            while (true)
+            {
+                Console.Write("Введите желаемую сумму накопления в рублях: ");
+
+                string input = Console.ReadLine();
+                input = input.Replace('.', ',');
+
+                if (double.TryParse(input, out double result))
+                {
+                    if (result > 0 && result > contribution)
+                        return result;
+
+                    if (result <= 0)
+                    {
+                        Console.WriteLine("Введите положительное значение!");
+                    }
+                    else if (result <= contribution)
+                    {
+                        Console.WriteLine("Сумма должна быть больше взноса!");
+                    }
+                }
+                else
+                    Console.WriteLine("Неверный формат");
+            }
+        }
+
+        private static double SetParam(string text)
+        {
+            while (true)
+            {
+                Console.Write(text);
+
+                string input = Console.ReadLine();
+                input = input.Replace('.', ',');
+
+                if (double.TryParse(input, out double param))
+                {
+                    if (param > 0)
+                        return param;
+
+                    Console.WriteLine("Введите положительное значение!");
+                }
+                else
+                    Console.WriteLine("Неверный формат");
+            }
+        }
+    }
+}
