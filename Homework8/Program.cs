0b90895aaa45c92538c4c60e94923fa80de131be
Rayle1gh <darkmaxim003@gmail.com>
2019-11-26 15:47:22 +0300

    ДЗ 8

---
+++ Homework8/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine($"Введите имя человека {i}");
+                string name = Console.ReadLine();
+
+            inputAge:
+                Console.WriteLine($"Введите возраст {name}");
+
+                if (int.TryParse(Console.ReadLine(), out int age))
+                {
+                    var person = new Person
+                    {
+                        Name = name,
+                        Age = age
+                    };
+
+                    people.Add(person);
+                }
+                else
+                    goto inputAge;
+            }
+            people.ForEach((p) =>
+            {
+                Console.WriteLine(p.Result);
+            });
+            Console.ReadKey();
+        }
+    }
+
+    class Person
+    {
+        public int Age { get; set; }
+
+        public string Name { get; set; }
+
+        public int AgeIn4Years
+        {
+            get
+            {
+                return Age + 4;
+            }
+        }
+
+        public string Result
+        {
+            get
+            {
+                return $"Имя: {Name}, Возраст через 4 года: {AgeIn4Years}";
+            }
+        }
+    }
+}
