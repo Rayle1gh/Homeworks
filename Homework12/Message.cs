6bbe7772d17044f552ff0603a56e5905ff4d790a
Rayle1gh <darkmaxim003@gmail.com>
2019-12-17 16:09:44 +0300

    ДЗ 12

---
+++ Homework12/Message.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework12
+{
+    public class Message
+    {
+        public Message(string subject, string text)
+        {
+            Subject = subject;
+            Text = text;
+        }
+
+        public string Subject { get; private set; }
+
+        public string Text { get; private set; }
+
+        public DateTime SendTime { get; set; }
+
+        public DateTime ReciveTime { get; set; }
+
+        public User Sender { get; set; }
+
+        public User Reciver { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Тема: {0}\nСообщение: {1}", Subject, Text);
+        }
+    }
+}
---
+++ Homework12/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Homework12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User userMax = new User("Max");
+            User userIgor = new User("Igor");
+
+            Message message = new Message("Hi", "Hello");
+
+            userMax.SendMessage(userIgor, message);
+        }
+    }
+}
